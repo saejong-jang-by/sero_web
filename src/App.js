ad4c281f1b06ebc05d90acce94848d563e0153bf
saejongjang <youthrich40@gmail.com>
2021-09-20 21:19:05 -0600

    updated UI

--- src/App.js
+++ src/App.js
@@ -6,9 +6,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-			<h1>Sero Language</h1>
-			<img src={mainLogo} height='300px' width='300px'/>
-			<a style={{textAlign:`center`}} href={'https://byu.az1.qualtrics.com/jfe/form/SV_9SvEcvBwJCH39fU'}>Survey</a>
+			<section>
+				<img src={mainLogo} height='300px' width='300px'/>
+			</section>
+
+			<section style={{backgroundColor: `#F0F0F0`}}>
+				<br/>
+				<br/>
+				<br/>
+				<br/>
+				<h1 style={{height: `15rem`, textAlign:`center`, color: `#BEBEBE`, "font-size": `5rem`}}>Sero Language</h1>
+			</section>
+
+			<section>
+				<h4>Help us build better platform for all the language learners!</h4>
+				<h1><a style={{textAlign:`center`, "text-decoration": `none`}} href={'https://byu.az1.qualtrics.com/jfe/form/SV_9SvEcvBwJCH39fU'}>Take a survey</a></h1>
+			</section>
     </div>
   );
 }
