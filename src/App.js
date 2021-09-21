3a21e1f2169c9938138a23980ee64edf1b13411b
saejongjang <youthrich40@gmail.com>
2021-09-20 20:50:56 -0600

    updated link and logo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import logo from './logo.svg';
 import mainLogo from'./assets/sero_logo.png';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-			Name: Sero Language
-			<br/>
-			Logo:
-			<br/>
+			<h1>Sero Language</h1>
 			<img src={mainLogo} height='300px' width='300px'/>
+			<a style={{textAlign:`center`}} href={'https://byu.az1.qualtrics.com/jfe/form/SV_9SvEcvBwJCH39fU'}>Survey</a>
     </div>
   );
 }
